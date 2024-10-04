c99ef1dce4098acb3b501ea877513ba7a5fca36a
André_Rodrigues <135463209+tommatti99@users.noreply.github.com>
2024-10-04 16:43:30 -0300

    fix: added cors config

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,9 +5,21 @@ pub mod collect_api;
 pub mod collect_models;
 
 #[macro_use] extern crate rocket;
+use rocket_cors::{AllowedOrigins, CorsOptions};
+use rocket::http::Method;
+
+
 
 #[launch]
 fn rocket() -> _ {
+    let cors = CorsOptions::default()
+        .allowed_methods(vec![Method::Post].into_iter().map(From::from).collect())
+        .allow_credentials(true)
+        .to_cors()
+        .expect("ERROR AT CORS");
+
+    
     rocket::build()
         .mount("/api", routes![collect_api::collect_data_api])
+        .attach(cors)
 }
