29288f25865e6f9b45482f88e2611b8f3b2d5faf
Andre_Rodrigues <135463209+tommatti99@users.noreply.github.com>
2024-10-31 18:11:36 -0300

    Update form.rs

--- frontend/src/components/form.rs
+++ frontend/src/components/form.rs
@@ -4,13 +4,6 @@ use yew::prelude::*;
 use reqwest;
 use serde::Deserialize;
 use wasm_bindgen_futures::spawn_local;
-use lazy_static::lazy_static;
-
-
-lazy_static! {
-    static ref LANDING_PAGE_API_LINK: String = env::var("LANDING_PAGE_API")
-        .expect("API MUST BE SET");
-}
 
 struct LandingPageRequest {
     name: String,
@@ -139,6 +132,7 @@ pub fn Form() -> Html {
 }
 
 async fn send_request_to_api(request: LandingPageRequest) -> LandingPageResponse {
+    let landing_page_api = "https://landing-page-rs-backend.onrender.com/api/landing_page";
     
     let client = reqwest::Client::new();
     let mut body_map =  HashMap::new();
@@ -149,7 +143,7 @@ async fn send_request_to_api(request: LandingPageRequest) -> LandingPageResponse
     body_map.insert("want_to_receive_more_info", request.want_to_receive_more_info.to_string());
     
     match client
-        .post(&*LANDING_PAGE_API_LINK)
+        .post(landing_page_api_link)
         .json(&body_map)
         .send()
         .await {
